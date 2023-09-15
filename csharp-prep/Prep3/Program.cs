ed85d564305efe092e5439ffe9b3644ca7105165
Mitch Coleman <col19028@byui.edu>
2023-09-15 11:27:46 -0600

    prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGen = new Random();
+        int number = randomGen.Next (1,101);
+
+        int guess = -1;
+
+        while (guess != number)
+        {
+            Console.Write("What is the magic number? ");
+            guess = int.Parse(Console.ReadLine());
+
+            
+            if (number > guess)
+            {
+                Console.WriteLine("Too low");
+            }
+            
+            else if (number < guess)
+            {
+                Console.WriteLine("Too High");
+            }
+            else
+            {
+                Console.WriteLine("You've guessed it!");
+            }
+        }
+        
+
+
+
+
+
+
+
+
+
     }
 }
