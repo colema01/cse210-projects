21a975f13fd87805a51c180a9d553b02cb4111b8
Mitch Coleman <col19028@byui.edu>
2023-10-26 06:25:44 -0700

    change

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,22 @@
 using System;
 
+using System;
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+    
+        Assignment a1 = new Assignment("Mitch Coleman", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+       
+        MathAssignment a2 = new MathAssignment("Clayton Hailey", "Fractions", "4.0", "12-22");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Megan Kerr", "Europe", "The Causes of World War II");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
