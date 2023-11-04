91bf476cae07e4045e5d986b72065f83ef9202f2
Mitch Coleman <col19028@byui.edu>
2023-11-04 15:33:13 -0600

    chagne

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -2,8 +2,23 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Develop04 World!");
+        Console.WriteLine("Welcome to the Activity Program!");
+        ActivityManager activityManager = new ActivityManager();
+
+        while (true)
+        {
+            Console.WriteLine("Choose an activity:");
+            Console.WriteLine("1. Breathing");
+            Console.WriteLine("2. Reflection");
+            Console.WriteLine("3. Exit");
+
+            string input = Console.ReadLine();
+            if (!activityManager.HandleInput(input))
+            {
+                break; // Exit the program when "3" is entered
+            }
+        }
     }
 }
