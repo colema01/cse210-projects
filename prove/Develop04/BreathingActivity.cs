759bba103d33981046df35f2a284d4c7013545a8
Mitch Coleman <col19028@byui.edu>
2023-11-04 15:40:18 -0600

    changes

--- prove/Develop04/BreathingActivity.cs
+++ prove/Develop04/BreathingActivity.cs
@@ -2,7 +2,7 @@ using System;
 
 public class BreathingActivity : Activity
 {
-    public BreathingActivity() : base("Breathing", "This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus on your breathing.") { }
+    public BreathingActivity() : base("Breathing", "This activity will help you relax by walking you through breathing in and out slowly.") { }
 
     protected override void RunActivity()
     {
